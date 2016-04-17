08ad9800997a6cd7f15b0bcc78b307209d67adc4
treamous <trea@treamous.com>
2016-04-17 14:16:31 -0500

    Add basic pooling functionality.

---
+++ pool.lua
@@ -0,0 +1,32 @@
+local pool = {}
+local poolmt = {__index = pool}
+
+function pool.create(newObject, poolSize)
+	poolSize = poolSize or 16
+	assert(newObject, "A function that returns new objects for the pool is required.")
+
+	freeObjects = {}
+	for i = 1, poolSize do
+		table.insert(freeObjects, newObject())
+	end
+
+	return setmetatable({
+			freeObjects = freeObjects,
+			newObject = newObject
+		},
+		poolmt
+	)
+end
+
+function pool:obtain()
+	return #self.freeObjects == 0 and self.newObject() or table.remove(self.freeObjects)
+end
+
+function pool:free(obj)
+	assert(obj, "An object to be freed must be passed.")
+
+	table.insert(self.freeObjects, obj)
+	if obj.reset then obj.reset() end
+end
+
+return pool
